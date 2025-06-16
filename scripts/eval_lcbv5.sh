856f6708e55f8937a955832b1a6497b88646aa84
Katrina Evtimova <kve216@nyu.edu>
2025-06-16 11:57:01 -0400

    Script for LiveCodeBenchv5 eval.

---
+++ scripts/eval_lcbv5.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export VLLM_WORKER_MULTIPROC_METHOD=spawn
+
+# === Configuration ===
+NUM_GPUS=1
+MODEL_NAME=Qwen2.5-1.5B-MoT-GRPO-2025-06-14-22-39
+MODEL=data/$MODEL_NAME
+TASK=livecodebenchv5
+OUTPUT_DIR=data/evals/$MODEL_NAME/$TASK
+
+MODEL_ARGS="model_name=$MODEL,dtype=bfloat16,max_model_length=32768,gpu_memory_utilization=0.8,data_parallel_size=$NUM_GPUS,generation_parameters={max_new_tokens:32768,temperature:0.6,top_p:0.95}"
+
+# === Run Evaluation ===
+lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
+    --use-chat-template \
+    --output-dir $OUTPUT_DIR
