ccd1048546fdbfca28b8bdf0c8c4afe74371586c
Katrina Evtimova <kve216@nyu.edu>
2025-06-16 13:48:17 -0400

    Eval script.

--- scripts/eval_lcbv5.sh
+++ scripts/eval_lcbv5.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
 export VLLM_WORKER_MULTIPROC_METHOD=spawn
+MODEL=data/Qwen2.5-1.5B-MoT-GRPO-2025-06-14-22-39/checkpoint-800/
+MODEL_ARGS="model_name=$MODEL,dtype=bfloat16,max_model_length=32768,gpu_memory_utilization=0.8,generation_parameters={max_new_tokens:32768,temperature:0.6,top_p:0.95}"
+OUTPUT_DIR=data/evals/$MODEL
 
-# === Configuration ===
-NUM_GPUS=1
-MODEL_NAME=Qwen2.5-1.5B-MoT-GRPO-2025-06-14-22-39/checkpoint-2200
-MODEL=data/$MODEL_NAME
-TASK=livecodebenchv5
-OUTPUT_DIR=data/evals/$MODEL_NAME/$TASK
-
-MODEL_ARGS="model_name=$MODEL,dtype=bfloat16,max_model_length=32768,gpu_memory_utilization=0.8,data_parallel_size=$NUM_GPUS,generation_parameters={max_new_tokens:32768,temperature:0.6,top_p:0.95}"
-
-# === Run Evaluation ===
-lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
-    --use-chat-template \
-    --output-dir $OUTPUT_DIR \
-    --no-check-hub
+lighteval vllm $MODEL_ARGS "extended|lcb:codegeneration|0|0" \
+--use-chat-template \
+--output-dir $OUTPUT_DIR
