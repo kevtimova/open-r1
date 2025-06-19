557457f7a554dcd1e9634a7aaadba2bc17720e6f
Katrina Evtimova <kve216@nyu.edu>
2025-06-18 22:44:28 -0400

    Test eval during training.

--- src/open_r1/grpo.py
+++ src/open_r1/grpo.py
@@ -132,6 +132,16 @@ def main(script_args, training_args, model_args):
         processing_class=tokenizer,
     )
 
+    ##########
+    # Evaluate
+    ##########
+    if training_args.do_eval:
+        logger.info("*** Evaluate ***")
+        metrics = trainer.evaluate()
+        metrics["eval_samples"] = len(dataset[script_args.dataset_test_split])
+        trainer.log_metrics("eval", metrics)
+        trainer.save_metrics("eval", metrics)
+
 
     ###############
     # Training loop
