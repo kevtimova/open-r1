c3304c9be14ff6ff033c4ef164d84ede1ced3d6e
Katrina Evtimova <kve216@nyu.edu>
2025-06-20 16:52:03 -0400

    Fix model path.

--- scripts/finetune.sh
+++ scripts/finetune.sh
@@ -5,6 +5,6 @@ export RUN_ID=$(date +%Y%m%d_%H%M%S)
 ACCELERATE_LOG_LEVEL=info \
 accelerate launch --config_file recipes/accelerate_configs/zero3.yaml \
 src/open_r1/grpo.py \
---config recipes/Qwen2.5-1.5B/grpo/config_mot.yaml \
+--config recipes/Qwen2.5-1.5B-Instruct/grpo/config_mot.yaml \
 --output_dir "data/Qwen2.5-1.5B-MoT-GRPO-$RUN_ID" \
 --vllm_mode colocate
