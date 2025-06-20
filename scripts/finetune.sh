7b3cc1b3bb74031f8c6ba764a4e8d61720bae7d0
Katrina Evtimova <kve216@nyu.edu>
2025-06-19 22:32:57 -0400

    Fine-tuning script.

--- scripts/finetune.sh
+++ scripts/finetune.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# export RUN_ID=$(date +%Y%m%d_%H%M%S)
+export RUN_ID=$(date +%Y%m%d_%H%M%S)
 
 ACCELERATE_LOG_LEVEL=info \
 accelerate launch --config_file recipes/accelerate_configs/zero3.yaml \
 src/open_r1/grpo.py \
---config recipes/Qwen2.5-1.5B-Instruct/grpo/config_mot.yaml \
---output_dir "data/Qwen2.5-1.5B-MoT-GRPO-20250619_204928" \
---vllm_mode colocate \
---resume_from_checkpoint "data/Qwen2.5-1.5B-MoT-GRPO-20250619_204928/checkpoint-25/" 
+--config recipes/Qwen2.5-1.5B/grpo/config_mot.yaml \
+--output_dir "data/Qwen2.5-1.5B-MoT-GRPO-$RUN_ID" \
+--vllm_mode colocate
