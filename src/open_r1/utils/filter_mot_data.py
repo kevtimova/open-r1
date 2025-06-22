9a16bd18d3445a09c63456e2b9be3685ca46fb4d
Katrina Evtimova <kve216@nyu.edu>
2025-06-22 17:35:30 -0400

    Option to extend the validation set.

--- src/open_r1/utils/filter_mot_data.py
+++ src/open_r1/utils/filter_mot_data.py
@@ -1,4 +1,4 @@
-from datasets import load_dataset, DatasetDict
+from datasets import load_dataset, Dataset
 import argparse
 from open_r1.utils import extract_programming_language, extract_test_cases
 
@@ -9,6 +9,7 @@ def filter_dataset(dataset_name="open-r1/Mixture-of-Thoughts",
                    split_sizes=[10000, 8000, 5000, 3000],
                    language="all",
                    test_size=10,
+                   test_repeat=1,
                    seed=42):
     """
     Load and filter the dataset based on the number of tokens.
@@ -18,6 +19,7 @@ def filter_dataset(dataset_name="open-r1/Mixture-of-Thoughts",
     if language != "all":
         # Filter the dataset by programming language if specified
         ds = ds.filter(lambda x: extract_programming_language(x["messages"][0]["content"]) == language)
+
     # Remove samples without test cases
     def has_test_cases(example):
         prompt = example["messages"][0]["content"]
@@ -32,6 +34,13 @@ def filter_dataset(dataset_name="open-r1/Mixture-of-Thoughts",
 
         # Randomly split the dataset into train and test sets
         filtered_ds = filtered_ds.train_test_split(test_size=test_size, seed=seed)
+
+        # Repeat test set sevearal times to get more samples
+        if test_repeat > 1:
+            new_test = {}
+            for key in filtered_ds["test"].features.keys():
+                new_test[key] = filtered_ds["test"][key] * test_repeat
+            filtered_ds["test"] = Dataset.from_dict(new_test)
 
         # Save to local disk
         if language == "all":
@@ -48,8 +57,9 @@ if __name__ == "__main__":
     parser.add_argument("--split", type=str, default="train", help="Dataset split to filter.")
     parser.add_argument("--split_sizes", type=str, default="10000,8000,5000,3000", help="Comma-separate list of number of tokens to filter by.")
     parser.add_argument("--test_size", type=int, default=10, help="Number of test samples in split.")
-    parser.add_argument("--seed", type=int, default=42, help="Random seed for reproducibility.")
+    parser.add_argument("--test_repeat", type=int, default=1, help="Number of times to repeat the test set.")
     parser.add_argument("--language", type=str, default="all", help="Programming language to filter by (default: all).")
+    parser.add_argument("--seed", type=int, default=42, help="Random seed for reproducibility.")
     args = parser.parse_args()
 
     # Call the filter function with parsed arguments
@@ -60,5 +70,6 @@ if __name__ == "__main__":
                    split=args.split,
                    split_sizes=split_sizes,
                    test_size=args.test_size,
+                   test_repeat=args.test_repeat,
                    seed=args.seed)
     
