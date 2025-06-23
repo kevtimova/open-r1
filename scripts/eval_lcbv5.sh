230100381971ab8c97e71b8141fb2a4040f978c8
Katrina Evtimova <kve216@nyu.edu>
2025-06-23 09:22:33 -0400

    Eval script.

--- scripts/eval_lcbv5.sh
+++ scripts/eval_lcbv5.sh
@@ -3,8 +3,8 @@
 NUM_GPUS=8
 
 export VLLM_WORKER_MULTIPROC_METHOD=spawn
-MODEL_NAME=Qwen2.5-7B-Instruct
-MODEL=Qwen/$MODEL_NAME
+MODEL_NAME=Qwen2.5-7B-MoT-GRPO-20250623_003621/checkpoint-10
+MODEL=/mnt/ai_researcher_volume_nyc2_1750677290711/data/$MODEL_NAME
 MODEL_ARGS="model_name=$MODEL,dtype=bfloat16,max_model_length=32768,data_parallel_size=$NUM_GPUS,gpu_memory_utilization=0.7,generation_parameters={max_new_tokens:4096,temperature:0.6,top_p:0.95}"
 OUTPUT_DIR=data/evals/$MODEL_NAME
 
