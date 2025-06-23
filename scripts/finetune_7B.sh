22b2207a6afe98d88f432734ced01360240a543c
Katrina Evtimova <kve216@nyu.edu>
2025-06-22 20:36:08 -0400

    Finetuning 7B.

--- scripts/finetune_7B.sh
+++ scripts/finetune_7B.sh
@@ -5,6 +5,6 @@ export RUN_ID=$(date +%Y%m%d_%H%M%S)
 ACCELERATE_LOG_LEVEL=info \
 accelerate launch --config_file recipes/accelerate_configs/zero3.yaml \
 src/open_r1/grpo.py \
---config recipes/Qwen2.5-7B/grpo/config_mot_revised.yaml \
+--config recipes/Qwen2.5-7B/config_mot_revised.yaml \
 --output_dir "data/Qwen2.5-7B-MoT-GRPO-$RUN_ID" \
 --vllm_mode colocate
