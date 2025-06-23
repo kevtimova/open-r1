cdab815e285488e04c04c2a2494f13beefc8e281
Katrina Evtimova <kve216@nyu.edu>
2025-06-23 16:26:47 -0400

    Update the evaluation script.

--- src/open_r1/grpo_eval.py
+++ src/open_r1/grpo_eval.py
@@ -28,8 +28,54 @@ from open_r1.utils.callbacks import get_callbacks
 from open_r1.utils.wandb_logging import init_wandb_training
 from trl import GRPOTrainer, ModelConfig, TrlParser, get_peft_config
 
-from open_r1.grpo import make_conversation
+# CUSTOM
+import wandb
+from transformers import Trainer
+from trl.trainer.callbacks import _generate_completions, gather_object
+from trl.trainer.callbacks import maybe_apply_chat_template, Optional, GenerationConfig
+import pandas as pd
 
+class LogCompletions:
+
+    def __init__(
+        self,
+        trainer: Trainer,
+        generation_config: Optional[GenerationConfig] = None,
+        eval_dataset = None,
+    ):
+        self.trainer = trainer
+        self.generation_config = generation_config
+        self.table = []
+
+        eval_dataset = eval_dataset or self.trainer.eval_dataset
+        if eval_dataset is None:
+            raise ValueError()
+        self.eval_dataset = eval_dataset
+
+    def run(self, batch_size, tokenizer):
+        tokenizer.padding_side = "left"
+        accelerator = self.trainer.accelerator
+        model = self.trainer.model_wrapped
+        prompts = self.eval_dataset["prompt"]
+        prompts = [maybe_apply_chat_template({"prompt": prompt}, tokenizer)["prompt"] for prompt in prompts]
+        completions = _generate_completions(
+            prompts,
+            model=model,
+            tokenizer=tokenizer,
+            accelerator=accelerator,
+            generation_config=self.generation_config,
+            batch_size=batch_size,
+        )
+        # TODO: Add multi-gpu support.
+        completions = gather_object(completions)
+        prompts = gather_object(prompts)
+
+        # Build the data to log
+        data = list(zip(prompts, completions))
+        self.table.extend(data)
+        table = pd.DataFrame(columns=["prompt", "completion"], data=self.table)
+
+        wandb.log({"completions": table})
 
 logger = logging.getLogger(__name__)
 
@@ -70,20 +116,11 @@ def main(script_args, training_args, model_args):
         logger.info(f"Checkpoint detected, resuming training at {last_checkpoint=}.")
 
     if "wandb" in training_args.report_to:
+        wandb.init()
         init_wandb_training(training_args)
 
     # Load the dataset
     dataset = get_dataset(script_args)
-    dataset = dataset.map(make_conversation)
-    try:
-        for split in dataset:
-            if "messages" in dataset[split].column_names:
-                dataset[split] = dataset[split].remove_columns("messages")
-    except:
-        dataset = {'train': dataset}
-        for split in dataset:
-            if "messages" in dataset[split].column_names:
-                dataset[split] = dataset[split].remove_columns("messages")
 
     ################
     # Load tokenizer
@@ -98,7 +135,26 @@ def main(script_args, training_args, model_args):
 
     # Get reward functions from the registry
     reward_funcs = get_reward_funcs(script_args)
- 
+
+    DEMO = True
+
+    # Format into conversation
+    def make_conversation(example, prompt_column: str = script_args.dataset_prompt_column):
+        prompt = example[prompt_column][0]
+        # completion = example[prompt_column][1] # noqa
+        if DEMO:
+            prompt['content'] = "... " + prompt['content'][-100:]
+        return {"prompt": [prompt]}
+
+    dataset = dataset.map(make_conversation)
+
+    for split in dataset:
+        if "messages" in dataset[split].column_names:
+            dataset[split] = dataset[split].remove_columns("messages")
+
+    train_dataset = dataset[script_args.dataset_train_split]
+    eval_dataset = dataset[script_args.dataset_test_split] # TODO: Use actual eval set from local split.
+
     #############################
     # Initialize the GRPO trainer
     #############################
@@ -106,8 +162,8 @@ def main(script_args, training_args, model_args):
         model=model,
         reward_funcs=reward_funcs,
         args=training_args,
-        train_dataset=dataset[script_args.dataset_train_split],
-        eval_dataset=(dataset[script_args.dataset_test_split] if training_args.eval_strategy != "no" else None),
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset,
         peft_config=get_peft_config(model_args),
         callbacks=get_callbacks(training_args, model_args),
         processing_class=tokenizer,
@@ -118,10 +174,34 @@ def main(script_args, training_args, model_args):
     ##########
     if training_args.do_eval:
         logger.info("*** Evaluate ***")
-        metrics = trainer.evaluate()
-        metrics["eval_samples"] = len(dataset[script_args.dataset_test_split])
-        trainer.log_metrics("eval", metrics)
-        trainer.save_metrics("eval", metrics)
+        # metrics = trainer.evaluate()
+        # metrics["eval_samples"] = len(eval_dataset)
+        # trainer.log_metrics("eval", metrics)
+        # trainer.save_metrics("eval", metrics)
+        if trainer.accelerator.is_main_process:
+            # self.vllm_client.generate(
+            #                 prompts=ordered_set_of_prompts,
+            #                 n=self.num_generations,
+            #                 repetition_penalty=self.repetition_penalty,
+            #                 temperature=self.temperature,
+            #                 top_p=self.top_p,
+            #                 top_k=-1 if self.top_k is None else self.top_k,
+            #                 min_p=0.0 if self.min_p is None else self.min_p,
+            #                 max_tokens=self.max_completion_length,
+            #                 guided_decoding_regex=self.guided_decoding_regex,
+            #             )
+            generation_config = GenerationConfig(
+                do_sample=True,
+                repetition_penalty=training_args.repetition_penalty,
+                temperature=training_args.temperature,
+                max_tokens=training_args.max_completion_length,
+            )
+            if training_args.num_generations > 1:
+                # Duplicate the eval dataset per generation.
+                eval_dataset = eval_dataset.repeat(training_args.num_generations)
+
+            log_completions = LogCompletions(trainer, generation_config=generation_config, eval_dataset=eval_dataset)
+            log_completions.run(batch_size=training_args.per_device_eval_batch_size, tokenizer=tokenizer)
 
 
 if __name__ == "__main__":
